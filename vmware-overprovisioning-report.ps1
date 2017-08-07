0007460bec6d34ffe17adb406fcfe540d1e72bbe
Wylie Bayes <me@wyliebayes.com>
2017-08-07 13:17:30 -0600

    Update vmware-overprovisioning-report.ps1

--- vmware-overprovisioning-report.ps1
+++ vmware-overprovisioning-report.ps1
@@ -1,33 +1,33 @@
 
+### This updated version will total all VM disks on a datastore where the disk file name matches the datastore name.  I had to
+### update this from the previous version because datastore clustering and DRS would spread out VM disks between datastores which
+### ended up skewing my report data. :). 
+
 function vmware-provisioning {
-	$viservers = "vcenter1", "vcenter2", "vcenter3", "vcenter4"
-	$ss = "\\share\e$\Storage_Reports\"
-	$report = "C:\Vmware_Provisioning.csv"
+	$viservers = "vcenter1", "vcenter2", "vcenter3"
 	$date = (Get-Date).tostring("yyyyMMdd")
 	Add-PSSnapin Vmware.VIMAutomation.Core | Out-Null
 	set-PowerCLIConfiguration -invalidCertificateAction "ignore" -confirm:$false | out-null
-	remove-item $report
 	foreach ($server in $viservers) {
 		$datastores = $null
-		$vms = $null
-		$size = $null
 		$output = $null
-		$vm = $null
 		$store = $null
-		connect-viserver $server | out-null
-		$datastores = get-datastore
+		connect-viserver $server -credential $cred| out-null
+		$datastores = get-datastore | sort Name
 		foreach ($store in $datastores) {
+			$disks = $null
 			$output = $null
-			$vms = get-vm -Datastore $store
-			$size = $vms | Measure-Object ProvisionedSpaceGB -Sum | Select -expand Sum
+			$vmmemory = get-vm -Datastore $store.name | Measure-Object MemoryGB -Sum | Select -expand Sum
+			$disks = get-datastore $store | get-vm | get-harddisk | where {$_.Persistence -eq "Persistent" -and $_.Filename -match $store.name} | Measure-Object CapacityGB -Sum | Select -expand Sum
+			$total = $disks + $vmmemory
 			$output = New-Object PSobject -Property @{
-				"Name" = $store.name
-				"Provisioned" = $size
-				"Total Size" = $store.CapacityGB
-			} | Select Name, Provisioned, 'Total Size' | export-csv C:\VMware_Provisioning.csv -Append
+					"Name" = $store.name
+					"Provisioned" = $total
+					"Total Size" = $store.CapacityGB
+					"Difference in GB" = $store.CapacityGB - $total
+				} | Select Name, Provisioned, 'Total Size', 'Difference in GB' | export-csv C:\VMware_Provisioning.csv -Append
 		}
 		disconnect-viserver $server -confirm:$false -force | out-null
 		start-sleep -Seconds 3
 	}
-	copy-item $report -Destination "$ss'VMware_Provisioning_'$date.csv"
 }
