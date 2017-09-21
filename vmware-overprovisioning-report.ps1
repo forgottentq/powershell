3d0b749ad40b4e17a0c5ad73008487e15a4f81b7
Wylie Bayes <me@wyliebayes.com>
2017-09-21 09:05:26 -0600

    Update vmware-overprovisioning-report.ps1

--- vmware-overprovisioning-report.ps1
+++ vmware-overprovisioning-report.ps1
@@ -5,27 +5,34 @@
 
 function vmware-provisioning {
 	$cred = get-credential
-	$viservers = "vcenter1", "vcenter2", "vcenter3"
+	$viservers = "vcenter1", "vcenter2"
 	$date = (Get-Date).tostring("yyyyMMdd")
 	Add-PSSnapin Vmware.VIMAutomation.Core | Out-Null
 	set-PowerCLIConfiguration -invalidCertificateAction "ignore" -confirm:$false | out-null
 	foreach ($server in $viservers) {
 		$datastores = $null
+		$vms = $null
+		$size = $null
 		$output = $null
+		$vm = $null
 		$store = $null
-		connect-viserver $server -credential $cred| out-null
-		$datastores = get-datastore | sort Name
+		connect-viserver $server -credential $cred | out-null
+		$datastores = get-datastore
 		foreach ($store in $datastores) {
-			$disks = $null
+			$size = $null
 			$output = $null
-			$vmmemory = get-vm -Datastore $store.name | Measure-Object MemoryGB -Sum | Select -expand Sum
-			$disks = get-datastore $store | get-vm | get-harddisk | where {$_.Persistence -eq "Persistent" -and $_.Filename -match $store.name} | Measure-Object CapacityGB -Sum | Select -expand Sum
-			$total = $disks + $vmmemory
+			$vms = $null
+			$vms = get-vm -Datastore $store
+			foreach ($vm in $vms){
+				$vmsize = $null
+				$vmsize = $vm | get-harddisk | where {$_.Persistence -eq "Persistent"} | Measure-Object CapacityGB -Sum | Select -expand Sum
+				$size += $vmsize
+			}
 			$output = New-Object PSobject -Property @{
 					"Name" = $store.name
-					"Provisioned" = $total
+					"Provisioned" = $size
 					"Total Size" = $store.CapacityGB
-					"Difference in GB" = $store.CapacityGB - $total
+					"Difference in GB" = $store.CapacityGB - $size
 				} | Select Name, Provisioned, 'Total Size', 'Difference in GB' | export-csv C:\VMware_Provisioning.csv -Append
 		}
 		disconnect-viserver $server -confirm:$false -force | out-null
