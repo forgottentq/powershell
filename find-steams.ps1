c7c8d1e5d9826aa51eed9651ae2f2e2e7ca745c2
Wylie Bayes <me@wyliebayes.com>
2018-01-14 17:59:42 -0700

    Create find-steams.ps1
    Function to find non-default alternate data streams located on NTFS files.

---
+++ find-steams.ps1
@@ -0,0 +1,14 @@
+function find-streams($location) {
+	$files = get-childitem -Recurse -Path $location -erroraction SilentlyContinue
+	foreach ($file in $files){
+		$streams = get-item -path $file.Fullname -stream * | where {$_.Stream -ne ':$DATA' -and $_.Stream -ne 'Zone.Identifier'}
+		if ($streams -ne $false){
+			foreach ($stream in $streams){
+				#Do other stuff.. Write to a sperate file.. Create a custom psobject and export to CSV at the end.. etc.
+				write-host "$($file.Fullname) contains non-standard streams"
+				get-content -path $file.fullname -stream $stream.Stream
+				;""
+			}
+		}
+	}
+}
